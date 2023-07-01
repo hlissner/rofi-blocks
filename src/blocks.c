2fd5825d9f70341eb9953d5b03930a6f9615bf86
Henrik Lissner <git@henrik.io>
2023-07-01 19:35:24 +0200

    refactor: emit dynamic version + rofi version at INIT

--- src/blocks.c
+++ src/blocks.c
@@ -3,6 +3,7 @@
 
 #define G_LOG_DOMAIN "BlocksMode"
 
+#include <config.h>
 #include <sys/types.h>
 #include <signal.h>
 #include <fcntl.h>
@@ -346,10 +347,7 @@ static int blocks_mode_init(Mode* sw) {
 
     pd->read_channel_watcher = g_io_add_watch(pd->read_channel, G_IO_IN, on_new_input, sw);
 
-    char abi[8];
-    snprintf(abi, 8, "%d", ABI_VERSION);
-    // TODO: Read version dynamically
-    blocks_mode_private_data_write_to_channel(pd, Event__INIT, "0.1.0", abi);
+    blocks_mode_private_data_write_to_channel(pd, Event__INIT, PACKAGE_VERSION, ROFI_PACKAGE_VERSION);
     return TRUE;
 }
 
